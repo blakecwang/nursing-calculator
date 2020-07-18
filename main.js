b0539cfa6926bb619745b4c7522ccbbdeab0c572
Blake Wang <bwang@intouchhealth.com>
2020-07-18 10:03:27 -0700

    Remove MomentJS stuff

--- main.js
+++ main.js
@@ -1,29 +1,31 @@
 $(function() {
   $("#my-form").submit(function(event) {
-    var firstFeed = moment.duration($("#first-feed").val(), "HH:mm");
-    var lastFeed = moment.duration($("#last-feed").val(), "HH:mm");
-    var elapsed = lastFeed.subtract(firstFeed);
-    var decElapsed = elapsed.hours() + elapsed.minutes() / 60;
+    // Parse the form inputs.
+    var firstFeed = Date.parse(
+      '01 Jan 1970 ' + $("#first-feed").val() + ':00 GMT'
+    );
+    var lastFeed = Date.parse(
+      '01 Jan 1970 ' + $("#last-feed").val() + ':00 GMT'
+    );
 
-    var f = firstFeed.format("HH:mm");
-    console.log(f.constructor.name);
-    alert('hello');
+    // Calculate elapsed time.
+    var elapsed = (lastFeed - firstFeed) / 3600000;
 
+    // Calculate the best feeding interval.
     var prevSlop = 1001
     var slop = 1000
     var numFeedings = 0
     while (slop < prevSlop) {
       numFeedings += 1
       prevSlop = slop;
-      slop = Math.abs(3 - decElapsed / numFeedings);
+      slop = Math.abs(3 - elapsed / numFeedings);
     }
+    var interval = elapsed / (numFeedings - 1);
+    alert(interval);
 
-    var interval = decElapsed / (numFeedings - 1);
-    var mInterval = moment.duration(interval, 'hours')
-
-    var times = [];
-
-
+    //    var times = [];
+    //
+    //
     event.preventDefault();
   });
 });
